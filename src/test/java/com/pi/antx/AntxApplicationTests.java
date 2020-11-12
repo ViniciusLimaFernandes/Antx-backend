c2b59cfcadab819fe77a50ad755ce7e2fc7c2417
ViniciusLimaFernandes <viniciuslima.4k@gmail.com>
2020-11-12 20:38:13 -0300

    Application structure

---
+++ src/test/java/com/pi/antx/AntxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pi.antx;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AntxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
